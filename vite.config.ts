8cdd0236161c84e11c27c6016331913119267123
TsukemonoGit <mine43try.outlook.com>
2023-06-14 08:38:18 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
+    SvelteKitPWA({
+      strategies: "generateSW",
+      useCredentials: true,
+      manifest: {
+        name: `nostr-bookmark-viewer3`,
+        short_name: "nbv3",
+        theme_color: "#d9fa6b",
+        icons: [
+          {
+            src: "favicon.png",
+            type: "image/png",
+            purpose: "any",
+            sizes: "48x48, 144x144",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
 });
