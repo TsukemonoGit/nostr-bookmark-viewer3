02086fc1138342dc08c68800fe8d2897b13ae6a8
TsukemonoGit <mine43try.outlook.com>
2023-07-09 22:11:38 +0900

    fix

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -345,12 +345,6 @@ export async function getOgp(url: string): Promise<ogp> {
     // 指定したURLをもとにAPIからHTMLコンテンツを取得
     const res = await axios.get(
       `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`,
-      {
-        headers: {
-          'Access-Control-Allow-Origin':
-            'https://nostr-bookmark-viewer3.vercel.app',
-        },
-      },
     );
 
     // HTMLコンテンツをパースしてDOMツリーを作成
