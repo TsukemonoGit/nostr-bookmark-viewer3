0075aff9b94f12f27a1776f0e7b62475efee4819
TsukemonoGit <mine43try.outlook.com>
2023-07-09 21:16:32 +0900

    fix

--- src/lib/components/OGP.svelte
+++ src/lib/components/OGP.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   export let ogp: { title: string; image: string; description: string };
   export let url: string;
+
+  const urlUrl = new URL(url);
+  const type =
+    urlUrl.hostname === 'www.youtube.com' || urlUrl.hostname === 'm.youtube.com'
+      ? 'youtube'
+      : 'other';
 </script>
 
 <div
@@ -9,11 +15,23 @@
   <a class="" href={url} target="_blank">
     <div class="grid grid-cols-[auto_1fr] gap-0.5">
       <div class="overflow-hidden max-h-[6rem] relative">
-        <img
-          class="object-contain w-full max-h-[6rem]"
-          src={ogp.image}
-          alt=""
-        />
+        {#if type === 'youtube'}
+          <iframe
+            width="160"
+            height="90"
+            src="https://www.youtube.com/embed/{url}"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          />
+        {:else}
+          <img
+            class="object-contain w-full max-h-[6rem]"
+            src={ogp.image}
+            alt=""
+          />
+        {/if}
         <div
           class="absolute top-0 left-0 flex items-center justify-center p-1 bg-white rounded-full shadow"
         >
@@ -37,7 +55,7 @@
         class="p-1 border border-1 grid grid-rows-[auto_1fr] z-10 bg-primary-200 min-w-[12em]"
       >
         <div
-          class=" line-clamp-2 overflow-y-hidden text-sm font-bold text-primary-800 underline decoration-primary-600"
+          class="line-clamp-2 overflow-y-hidden text-sm font-bold text-primary-800 underline decoration-primary-600"
         >
           {ogp.title}
         </div>
