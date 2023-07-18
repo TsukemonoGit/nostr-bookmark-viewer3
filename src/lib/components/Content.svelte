923a5fd6b7a3f40e776d69a99f20266c353b42b6
TsukemonoGit <mine43try.outlook.com>
2023-07-18 22:40:42 +0900

    fix

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -133,48 +133,62 @@
       return content;
     }
   }
+  let view = false;
+  function clickView() {
+    view = true;
+  }
 </script>
 
-{#await getTextParts(text, tag)}
-  {text}
-{:then viewContent}
-  <div class="parent-container break-all whitespace-pre-wrap">
-    {#each viewContent as item, index}
-      {#if item.type === 'newline'}
-        <br />
-      {:else if item.type === 'space'}
-        &nbsp;
-      {:else if item.type === 'emoji'}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div
-          class="{item.marquee} w-[fit-content] break-all whitespace-pre-wrap inline-flex flex align-bottom"
-        >
-          {#if item.beforeSpace}{Array(item.beforeSpace)
-              .fill('\u00A0')
-              .join('')}{/if}
-          <img
-            class="max-h-[1.5em] inline object-contain align-bottom"
-            src={item.url}
-            alt=""
-            on:click={() => handleClickImage(item.url)}
-          />
-        </div>
-      {:else if item.type === 'url'}
-        {#if new URL(item.content).hostname.endsWith('twitter.com')}
-          <div class="max-h-[20rem] max-w-[36rem] overflow-auto">
-            <blockquote class="twitter-tweet">
-              <p lang="ja" dir="ltr">
-                <a class="anchor" href={item.content}>{item.content}</a>
-              </p>
-            </blockquote>
+{#if tag.some((tag) => tag[0] === 'content-warning') && view == false}
+  <button
+    type="button"
+    class="btn variant-filled-warning m-0"
+    on:click={clickView}
+  >
+    content-warning<br />
+    {tag[tag.findIndex((tag) => tag[0] === 'content-warning')][1]}
+  </button>
+{:else}
+  {#await getTextParts(text, tag)}
+    {text}
+  {:then viewContent}
+    <div class="parent-container break-all whitespace-pre-wrap">
+      {#each viewContent as item, index}
+        {#if item.type === 'newline'}
+          <br />
+        {:else if item.type === 'space'}
+          &nbsp;
+        {:else if item.type === 'emoji'}
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div
+            class="{item.marquee} w-[fit-content] break-all whitespace-pre-wrap inline-flex flex align-bottom"
+          >
+            {#if item.beforeSpace}{Array(item.beforeSpace)
+                .fill('\u00A0')
+                .join('')}{/if}
+            <img
+              class="max-h-[1.5em] inline object-contain align-bottom"
+              src={item.url}
+              alt=""
+              on:click={() => handleClickImage(item.url)}
+            />
+          </div>
+        {:else if item.type === 'url'}
+          {#if new URL(item.content).hostname.endsWith('twitter.com')}
+            <div class="max-h-[20rem] max-w-[36rem] overflow-auto">
+              <blockquote class="twitter-tweet">
+                <p lang="ja" dir="ltr">
+                  <a class="anchor" href={item.content}>{item.content}</a>
+                </p>
+              </blockquote>
 
-            <script
-              async
-              src="https://platform.twitter.com/widgets.js"
-              charset="utf-8"
-            ></script>
+              <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+                charset="utf-8"
+              ></script>
 
-            <!-- 
+              <!-- 
             <iframe
               title="twitter"
               frameborder="0"
@@ -182,51 +196,33 @@
               height="100%"
               src="https://twitframe.com/show?url={item.content}"
             /> -->
-          </div>
-        {:else if new URL(item.content).hostname === 'www.youtube.com' || new URL(item.content).hostname === 'm.youtube.com' || new URL(item.content).hostname === 'youtu.be'}
-          <iframe
-            class="rounded"
-            width="320"
-            height="180"
-            src={`https://www.youtube.com/embed/${pathname(item.content)}`}
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          />
-        {:else if item.content?.endsWith('.mp4')}
-          <video controls class="max-h-[20em]">
-            <source src={item.content} type="video/mp4" />
-            <track kind="captions" src="" label="English" default />
-            Your browser does not support the video tag.
-          </video>
-        {:else}
-          {#await loadOgp(item.content)}
-            <div
-              class="{item.marquee} w-[fit-content] break-all whitespace-pre-wrap inline-flex flex"
-            >
-              {#if item.beforeSpace}{Array(item.beforeSpace)
-                  .fill('\u00A0')
-                  .join('')}{/if}
-              <a class="anchor" href={item.content} target="_blank">
-                {#if item.content.length > 80}
-                  {item.content.slice(0, 75)}...
-                {:else}
-                  {item.content}
-                {/if}
-              </a>
             </div>
-          {:then ogp}
-            {#if $ogpStore[item.content].title !== ''}
-              <OGP ogp={$ogpStore[item.content]} url={item.content} />
-            {:else}
+          {:else if new URL(item.content).hostname === 'www.youtube.com' || new URL(item.content).hostname === 'm.youtube.com' || new URL(item.content).hostname === 'youtu.be'}
+            <iframe
+              class="rounded"
+              width="320"
+              height="180"
+              src={`https://www.youtube.com/embed/${pathname(item.content)}`}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            />
+          {:else if item.content?.endsWith('.mp4')}
+            <video controls class="max-h-[20em]">
+              <source src={item.content} type="video/mp4" />
+              <track kind="captions" src="" label="English" default />
+              Your browser does not support the video tag.
+            </video>
+          {:else}
+            {#await loadOgp(item.content)}
               <div
                 class="{item.marquee} w-[fit-content] break-all whitespace-pre-wrap inline-flex flex"
               >
                 {#if item.beforeSpace}{Array(item.beforeSpace)
                     .fill('\u00A0')
                     .join('')}{/if}
-                <a class="anchor flex" href={item.content} target="_blank">
+                <a class="anchor" href={item.content} target="_blank">
                   {#if item.content.length > 80}
                     {item.content.slice(0, 75)}...
                   {:else}
@@ -234,83 +230,102 @@
                   {/if}
                 </a>
               </div>
-            {/if}
-          {/await}
-        {/if}
-      {:else if item.type === 'image'}
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class=" {item.marquee} w-[fit-content] inline-flex">
-          {#if item.beforeSpace}{Array(item.beforeSpace)
-              .fill('\u00A0')
-              .join('')}{/if}
-          <img
-            class="max-h-[10em] object-contain"
-            src={item.content}
-            alt=""
-            on:click={() => handleClickImage(item.content)}
-          />
-        </div>
-      {:else if item.type === 'nostr' && item.url}
-        {#if decodeCheck(item.url)}
-          <QuoteContent encodedId={item.url} />
-        {:else}
-          <span class="text-black/80"> {item.content}</span>
-        {/if}
-      {:else if item.type === 'quote' && item.number !== undefined}
-        <!--引用タグの中身がパブキーの時-->
-        {#if tag[item.number][0] === 'p'}
-          <Metadata
-            queryKey={['metadata', tag[item.number][1]]}
-            pubkey={tag[item.number][1]}
-            let:metadata
-          >
-            <div slot="loading">
-              <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
-                {tag[item.number][1]}
-              </div>
-            </div>
-            <div slot="error">
-              <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
-                {tag[item.number][1]}
-              </div>
-            </div>
-
-            <div slot="nodata">
-              <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
-                {tag[item.number][1]}
-              </div>
-            </div>
-
-            <button
-              class="inline-flex text-sm text-black/80"
-              on:click={() => {
-                handleClickPubkey(metadata, tag[item.number][1]);
-              }}
+            {:then ogp}
+              {#if $ogpStore[item.content].title !== ''}
+                <OGP ogp={$ogpStore[item.content]} url={item.content} />
+              {:else}
+                <div
+                  class="{item.marquee} w-[fit-content] break-all whitespace-pre-wrap inline-flex flex"
+                >
+                  {#if item.beforeSpace}{Array(item.beforeSpace)
+                      .fill('\u00A0')
+                      .join('')}{/if}
+                  <a class="anchor flex" href={item.content} target="_blank">
+                    {#if item.content.length > 80}
+                      {item.content.slice(0, 75)}...
+                    {:else}
+                      {item.content}
+                    {/if}
+                  </a>
+                </div>
+              {/if}
+            {/await}
+          {/if}
+        {:else if item.type === 'image'}
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <div class=" {item.marquee} w-[fit-content] inline-flex">
+            {#if item.beforeSpace}{Array(item.beforeSpace)
+                .fill('\u00A0')
+                .join('')}{/if}
+            <img
+              class="max-h-[10em] object-contain"
+              src={item.content}
+              alt=""
+              on:click={() => handleClickImage(item.content)}
+            />
+          </div>
+        {:else if item.type === 'nostr' && item.url}
+          {#if decodeCheck(item.url)}
+            <QuoteContent encodedId={item.url} />
+          {:else}
+            <span class="text-black/80"> {item.content}</span>
+          {/if}
+        {:else if item.type === 'quote' && item.number !== undefined}
+          <!--引用タグの中身がパブキーの時-->
+          {#if tag[item.number][0] === 'p'}
+            <Metadata
+              queryKey={['metadata', tag[item.number][1]]}
+              pubkey={tag[item.number][1]}
+              let:metadata
             >
-              @<u>{JSON.parse(metadata.content).name}</u>
-            </button>
-          </Metadata>
-        {:else if tag[item.number][0] === 'e' || tag[item.number][0] === 'q'}
-          <!--引用タグの中身がイベントIDの時-->
-          <QuoteContent2 id={tag[item.number][1]} />
-        {:else if tag[item.number][0] === 't'}
-          <u>#{tag[item.number][1]}</u>
-        {:else}
-          {tag[item.number][1]}
-        {/if}
-      {:else if item.content.length > 0}
-        <div
-          class="{item.marquee}
+              <div slot="loading">
+                <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
+                  {tag[item.number][1]}
+                </div>
+              </div>
+              <div slot="error">
+                <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
+                  {tag[item.number][1]}
+                </div>
+              </div>
+
+              <div slot="nodata">
+                <div class="-mt-0.5 px-2 opacity-60 text-sm overflow-hidden">
+                  {tag[item.number][1]}
+                </div>
+              </div>
+
+              <button
+                class="inline-flex text-sm text-black/80"
+                on:click={() => {
+                  handleClickPubkey(metadata, tag[item.number][1]);
+                }}
+              >
+                @<u>{JSON.parse(metadata.content).name}</u>
+              </button>
+            </Metadata>
+          {:else if tag[item.number][0] === 'e' || tag[item.number][0] === 'q'}
+            <!--引用タグの中身がイベントIDの時-->
+            <QuoteContent2 id={tag[item.number][1]} />
+          {:else if tag[item.number][0] === 't'}
+            <u>#{tag[item.number][1]}</u>
+          {:else}
+            {tag[item.number][1]}
+          {/if}
+        {:else if item.content.length > 0}
+          <div
+            class="{item.marquee}
                           break-all
                           whitespace-pre-wrap w-[fix-content
                           ] inline-flex flex"
-        >
-          {#if item.beforeSpace}{Array(item.beforeSpace)
-              .fill('\u00A0')
-              .join('')}{/if}
-          {item.content}
-        </div>
-      {/if}
-    {/each}
-  </div>
-{/await}
+          >
+            {#if item.beforeSpace}{Array(item.beforeSpace)
+                .fill('\u00A0')
+                .join('')}{/if}
+            {item.content}
+          </div>
+        {/if}
+      {/each}
+    </div>
+  {/await}
+{/if}
