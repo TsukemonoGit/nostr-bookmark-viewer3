7ba941811c2bdaab75eb52b732e4c2ffc722a37d
TsukemonoGit <mine43try.outlook.com>
2023-09-06 22:40:50 +0900

    取り敢えずsettingのページ

--- .svelte-kit/types/src/routes/$types.d.ts
+++ .svelte-kit/types/src/routes/$types.d.ts
@@ -17,4 +17,6 @@ type LayoutParentData = EnsureDefined<{}>;
 export type PageServerData = null;
 export type PageData = Expand<PageParentData>;
 export type LayoutServerData = null;
-export type LayoutData = Expand<LayoutParentData>;
+export type LayoutLoad<OutputData extends OutputDataShape<LayoutParentData> = OutputDataShape<LayoutParentData>> = Kit.Load<LayoutParams, LayoutServerData, LayoutParentData, OutputData, LayoutRouteId>;
+export type LayoutLoadEvent = Parameters<LayoutLoad>[0];
+export type LayoutData = Expand<Omit<LayoutParentData, keyof Kit.AwaitedProperties<Awaited<ReturnType<typeof import('./proxy+layout.js').load>>>> & OptionalUnion<EnsureDefined<Kit.AwaitedProperties<Awaited<ReturnType<typeof import('./proxy+layout.js').load>>>>>>;
