f47b18647f98c2ad79d977e467132028a1ec862d
TsukemonoGit <mine43try.outlook.com>
2023-09-21 20:16:07 +0900

    サーチのときの表示あれこれ

--- src/lib/components/ModalAddNote.svelte
+++ src/lib/components/ModalAddNote.svelte
@@ -88,7 +88,10 @@
               "https://example.com/example.png"]<br />
               {$_('ModalAddNote.example')}2 [ "a",
               "30030:84b0c46ab699ac35eb2ca286470b85e081db2087cdef63932236c397417782f5:mono"
-              ]
+              ]<br />
+              <span class="text-warning-500"
+                >{$_('ModalAddNote.caution')}
+              </span>
             </article>
             <!-- Enable for debugging: -->
 
