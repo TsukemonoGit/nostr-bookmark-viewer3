4786d93ffd8185ca027fcc611b22574771e99056
TsukemonoGit <mine43try.outlook.com>
2023-10-05 21:07:11 +0900

    ルーティングやんや

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
+
+  onMount(() => {
+    if ($page.params.slug.startsWith('note1')) {
+      goto(`/n/${$page.params.slug}`);
+    } else if ($page.params.slug.startsWith('naddr')) {
+      goto(`/a/${$page.params.slug}`);
+    } else if ($page.params.slug.startsWith('nprofile')) {
+      goto(`/p/${$page.params.slug}`);
+    }
+  });
+</script>
