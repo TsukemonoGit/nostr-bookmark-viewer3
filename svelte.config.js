30e8bf9496ec88cce79784048dd47585351749b6
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-09-27 21:07:59 +0900

    raiburari update

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: vitePreprocess(),
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
